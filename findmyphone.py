f663a2b469d07385ed2ee2e037b24eb1b07f00d8
edgecrush3r <tony.tromp@gmail.com>
2016-12-21 18:51:51 +0100

    modified:   findmyphone.py modified:   templates.yaml

--- findmyphone.py
+++ findmyphone.py
@@ -12,6 +12,21 @@ devices = api.devices;
 
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
+def gmapaddress_from_gps(str_lat,str_lng):
+    str_ret = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=#LAT#,#LNG#&sensor=false';
+    str_ret = str_ret.replace("#LAT#", str_lat);
+    str_ret = str_ret.replace("#LNG#", str_lng);
+    return str_ret
+
+def get_location(str_lat, str_lng):
+    try:
+        r = requests.get(gmapaddress_from_gps(str_lat,str_lng) );
+        res=r.json();
+        return res['results'][0]['formatted_address'];
+    except requests.exceptions.RequestException as e:
+        None; # fall through
+    return None;
+
 @ask.launch
 def new_inst():
   return 'HELLO and WELCOME';
@@ -25,29 +40,20 @@ def FindiDeviceIntend(device):
     text = render_template('FindiDeviceIntend', device=device)
     return statement(text).simple_card('FindiDeviceIntend', text)
 
-def gmapaddress_from_gps(str_lat,str_lng):
-    str_ret = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=#LAT#,#LNG#&sensor=false';
-    str_ret = str_ret.replace("#LAT#", str_lat);
-    str_ret = str_ret.replace("#LNG#", str_lng);
-    return str_ret
+
 
 @ask.intent('LocateiDeviceIntend', convert={'device': 'LIST_OF_DEVICES'})
 def LocateiDeviceIntend(device):
     for i in range(0,len(devices.values())):
       if ( (devices[i].content['deviceClass'].lower()==device.lower()) and (devices[i].content['location'])) :
-        devices = api.devices;
         str_lat = repr(devices[i].content['location']['latitude']);
         str_lng = repr(devices[i].content['location']['longitude']);
         positioning = devices[i].content['location']['positionType'];
 
-        try:
-            r = requests.get(gmapaddress_from_gps(str_lat,str_lng) );
-            res=r.json();
-            location = res['results'][0]['formatted_address'];
+        location = get_location(str_lat, str_lng);
+        if location is not None:
             text = render_template('LocateiDeviceIntend', device=device, location=location);
             return statement(text).simple_card('LocateiDeviceIntend', text);
-        except requests.exceptions.RequestException as e:
-            None; # fall through
 
     text = render_template('UnableToLocateiDevice', device=device)
     return statement(text).simple_card('UnableToLocateiDevice', text);
